dd8a38bec39c2debf6cb4f20234c278fd55bddcd
PP <pedrosspedro@sapo.pt>
2024-03-18 12:20:55 +0000

    City and DataManip

---
+++ DataManip.cpp
@@ -0,0 +1,28 @@
+//
+// Created by pedrosspedro on 18-03-2024.
+//
+
+#include "DataManip.h"
+
+template<class T>
+DataManip<T>::DataManip() {}
+
+template<class T>
+void DataManip<T>::readCities() {
+
+}
+
+template<class T>
+void DataManip<T>::readStations() {
+
+}
+
+template<class T>
+void DataManip<T>::readReservoirs() {
+
+}
+
+template<class T>
+void DataManip<T>::readPipes() {
+
+}
--- Graph.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by edu on 18-03-2024.
-//
-
-#include "Graph.h"
